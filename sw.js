b8f5ce1f28c7d55dbb2c2e265a69f3e03f97ad6f
calvin <calvinchaochao@outlook.com>
2022-10-21 19:06:12 +0800

    ...

--- sw.js
+++ sw.js
@@ -4,13 +4,9 @@ self.addEventListener("install", (e) => {
       .open("letslearn")
       .then((cache) =>
         cache.addAll([
-          ".",
-          "./assets",
           "./assets/default-ad53eec0.css",
           "./assets/common-40c724d0.css",
           "./assets/dark-58dfb0c7.css",
-          "./assets/menifest.json",
-          "./index.js.map",
           "./index.js",
           "./index.html",
         ])
