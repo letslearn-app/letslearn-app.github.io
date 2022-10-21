a7b559f477dce77df831e400d440cd44d407ddf8
calvin <calvinchaochao@outlook.com>
2022-10-21 18:41:34 +0800

    ...

---
+++ sw.js
@@ -0,0 +1,26 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open("letslearn")
+      .then((cache) =>
+        cache.addAll([
+          ".",
+          "./assets",
+          "./assets/default-ad53eec0.css",
+          "./assets/common-40c724d0.css",
+          "./assets/dark-58dfb0c7.css",
+          "./assets/menifest.json",
+          "./index.js.map",
+          "./index.js",
+          "./index.html",
+        ])
+      )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
