5dc6619910970fccd5f10994007b57f4438b96d6
calvin <calvinchaochao@outlook.com>
2022-11-06 16:55:25 +0800

    ...

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
-self.addEventListener("install", (e) => {});
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open("letslearn")
+      .then((cache) =>
+        cache.addAll([
+          "./assets/default-ad53eec0.css",
+          "./assets/common-40c724d0.css",
+          "./assets/dark-58dfb0c7.css",
+          "./index.js",
+          "./index.html",
+        ])
+      )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
